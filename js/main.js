ebbda76e85288449a994918a2bd9c1cb20484b8c
telatkaya <telat@karatekin.edu.tr>
2021-09-13 16:28:28 +0300

    yeni videolar eklendi

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 
     $(document).ready(function(){
         var videoPlayer = $("#myVideo");
+        var questionsTitle = $(".questions h1");
+        var questionsList = $(".questions .list");
+
         videoPlayer.on("ended", function() {
             var v = $(this);
             videoPlayer.children("source").attr("src","assets/video/ocean.mp4")
@@ -12,6 +15,11 @@
         $(".new-game").on("click",function(){
             videoPlayer.css("display","block")
             videoPlayer.trigger('play');
-        })
+        });
+          
+        videoPlayer.on('ended', function() {
+            questionsTitle.html("Which response to the mom is the most appropriate?")
+            questionsList.html("<a>Resource Mom</a><a>Change Transform</a><a>Transfer Hospital</a><a>Transfer Hospital</a>")
+         });
       });
     
